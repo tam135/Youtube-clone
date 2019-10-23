755d104757378d99166fd9fa9321d410ce6199e4
Tam Phan <tnphan0204@gmail.com>
2019-10-23 02:26:43 -0400

    Added sidebar nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import Header from './containers/Header/Header'
+import React, { Component } from 'react'
+import Header from './containers/Header/Header';
+import Sidebar from './containers/Sidebar/SideBar';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <Header/>
+        <Sidebar/>
+      </>
+    )
+  }
 }
-
-export default App;
---
+++ src/containers/Sidebar/SideBar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import  SideBarItem  from "./SideBarItem/SideBarItem";
+import { Menu } from "semantic-ui-react";
+import "./SideBar.scss";
+
+export default class SideBar extends React.Component {
+         render() {
+           return (
+             <Menu
+               borderless
+               vertical
+               stackable
+               fixed="left"
+               className="side-nav"
+             >
+               <SideBarItem highlight={true} label="Home" icon="home" />
+               <SideBarItem label="Trending" icon="fire" />
+               <SideBarItem label="Subscriptions" icon="play" />
+               <SideBarItem label="Library" icon="folder" />
+               <SideBarItem label="History" icon="history" />
+               <SideBarItem label="Watch later" icon="clock" />
+               <SideBarItem label="Liked videos" icon="thumbs up" />
+               <SideBarItem label="Movies and Shows" icon="film" />
+               <SideBarItem label="Settings" icon="setting" />
+               <SideBarItem label="Report history" icon="flag" />
+               <SideBarItem label="Help" icon="help circle" />
+               <SideBarItem label="Send feedback" icon="comment" />
+             </Menu>
+           );
+         }
+       }
