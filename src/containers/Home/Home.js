20757fa4c18debfdebbf5b36472233f3cd867ece
Tam Phan <tnphan0204@gmail.com>
2019-10-24 03:01:49 -0400

    set up video grid

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
 import VideoPreview  from "../../components/VideoPreview/VideoPreview";
+import VideoGrid  from "../../components/VideoGrid/VideoGrid";
+import SideBar  from "../SideBar/SideBar";
 import './Home.scss';
 
 export default class Home extends Component {
     render() {
         return (
-            <div className='home'>
-                <VideoPreview/>
+          <>
+            <SideBar />
+            <div className="home">
+              <div className="responsive-video-grid-container">
+                <VideoGrid title="Trending" />
+                <VideoGrid title="Autos & Vehicles" hideDivider={true} />
+              </div>
             </div>
-        )
+          </>
+        );
     }
 }
