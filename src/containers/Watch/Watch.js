4fc9f0c6ccc55da912fe1a607634dde85bc19e40
Tam Phan <tnphan0204@gmail.com>
2019-10-25 02:38:22 -0400

    Set up related video components with next up video box

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 import './Watch.scss'
 import VideoPreview from '../../components/VideoPreview/VideoPreview'
+import RelatedVideos from '../../components/RelatedVideos/RelatedVideos';
 
 export default class Watch extends Component {
     render() {
         return (
-            <>
+            <RelatedVideos/>
+           /*  <>
                 <VideoPreview horizontal={true}/>
                 <VideoPreview/>
-            </>
+            </> */
         )
     }
 }
