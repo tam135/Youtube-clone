6bc810b953c00113d59d3c3a099538692fdbd1d1
Tam Phan <tnphan0204@gmail.com>
2019-10-29 23:43:21 -0400

    set up boilerplate for redux

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import "semantic-ui-css/semantic.min.css";
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore  from './store/configureStore';
+
+const store = configureStore();
 
 ReactDOM.render(
-<BrowserRouter>
-    <App/>
-</BrowserRouter>,
- document.getElementById('root'));
-
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>, document.getElementById('root'));
