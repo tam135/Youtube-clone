408a9d2c3742b37a0fb0642e39311039576176b9
Tam Phan <tnphan0204@gmail.com>
2019-11-03 20:11:35 -0500

    fetch popular videos saga

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,4 +1,19 @@
-import { all } from 'redux-saga/effects';
+import { all, call, put } from 'redux-saga/effects';
+import watchMostPopularVideos from './Video';
+
 export default function* () {
-    yield all([]);
+    yield all([
+      fork(watchMostPopularVideos)
+    ]);
+}
+
+export function* fetchEntity(request, entity, ...args) {
+  try {
+    const response = yield call(request);
+    // directly return the result object and throw away the headers and the status text here
+    // if status and headers are needed, then instead of returning response.result, just return response.
+    yield put(entity.success(response.result, ...args));
+  } catch (error) {
+    yield put(entity.failure(error, ...args));
+  }
 }
